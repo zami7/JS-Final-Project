f413fdcceb4bb448067bc9b698e6b77004c6e535
Zamil Rahman <102779273+zami7@users.noreply.github.com>
2025-02-22 13:25:41 +0530

    project upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var parent = document.querySelectorAll('.images-container img');
+  for (var i = 0; i < parent.length; i++) {
+  console.log(parent[i])
+  parent[i].setAttribute("tabindex","0" ,(i+1).toString());
+  }
+});
+
+function upDate(previewPic){
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = `url(${previewPic.src})`;
+    imageDiv.textContent = previewPic.alt;
+      {console.log("Mouse is over the image");}    
+    }
+    
+function unDo() {   
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('images/bg.jpg')"; 
+    imageDiv.textContent = "Hover over an image below to display here.";
+      {console.log("Mouse is out of the image");}
+    }	
 
